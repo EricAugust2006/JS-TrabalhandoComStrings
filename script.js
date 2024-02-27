0ab7b583638c2186d45e53f7e03b79dcec688cfa
Eric <ericaugust2006@gmail.com>
2024-02-27 11:01:55 -0300

    Concluído

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Exercício 1: Crie um programa que concatene dois nomes e retorne uma frase de saudação.");
+let nome1 = "João";
+let nome2 = "Silva";
+console.log(`Bom dia, ${nome1} ${nome2}!`);
+
+console.log("Exercício 2: Crie um programa que adicione uma string ao final de outra string e retorne a string resultante.Entrada: string1 = 'hello', string2 = 'world' Saída: 'helloworld'");
+let string1 = "Hello";
+let string2 = "World";
+console.log(string1 + string2);
+
+console.log("Exercício 3: Crie um programa que receba uma string em formato de número decimal e retorne o número inteiro equivalente.");
+let stringNumber = "12.5";
+console.log(parseInt(stringNumber));
+
+console.log("Exercício 4: Crie um programa que converta uma string em formato de número inteiro para um número inteiro.");
+let stringNumber2 = "12";
+console.log(parseInt(stringNumber2));
+
+console.log("Exercício 5: Crie um programa que converta duas strings em formato de número inteiro para um número inteiro, faça uma soma desses números e apresente o resultado");
+let stringNumber3 = "12";
+let stringNumber4 = "3";
+console.log(parseInt(stringNumber3) + parseInt(stringNumber4));
